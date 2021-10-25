d4f82a58c936e966842bca1da0657173ba56494d
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-10-24 22:21:26 -0300

    Atualização
    alterações nas configurações, models e no admin

--- Appescola/appnotas/models.py
+++ Appescola/appnotas/models.py
@@ -6,3 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length = 255)
     slug =  models.SlugField(max_length = 255, unique= True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+    body = models.TextField()  
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    
