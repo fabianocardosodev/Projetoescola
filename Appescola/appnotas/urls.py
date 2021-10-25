33d4e11d019e4676f70c98b5052092b6f3bb2a2d
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-10-24 23:23:07 -0300

    Ultima atualizaçao

--- Appescola/appnotas/urls.py
+++ Appescola/appnotas/urls.py
@@ -1,2 +1,11 @@
 from django.urls import path
 
+from . import views 
+
+app_name =  "appnotas"
+
+urlpatterns = [
+    path("", views.PostListView.as_view(), name="list")
+    path("<slug:slug>/", views.PostDetailView.as_view(), name="detail"),
+
+]
